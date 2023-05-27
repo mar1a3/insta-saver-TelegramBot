46af13799d8394228c414242cc00a532ff449334
capoweb <popytka.nomer1@bk.ru>
2023-05-27 19:48:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,61 @@
 
 
-# l = instaloader.Instaloader()
-# l.load_session_from_file('elena_kalugina475')
-# pp = input("Введите имя пользователя: ")
-# l.download_profile(pp, profile_pic_only=True)
+# webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-
-
-
-
-
-# пробный бот на instabot
-import instaloader
+# telebot
 import time
 import telebot
 from telebot import types
+import os
+import glob
+
 
 # элементы бота
-api = ''
+api = '5999499263:AAEwA-THiPgM0sBAX6GJaUR18oLZHlmQbMY'
 bot = telebot.TeleBot(api)
 
-# общие переменные
-arr = {}
-
-l = instaloader.Instaloader()
-l.load_session_from_file('elena_kalugina475')
+# элементы вэбрайвера
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,text='Welcome to the instaH3lperBot',parse_mode="Markdown")
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    keyboard.add(types.KeyboardButton('Показать Ваших подписчиков'))
+    keyboard.add(types.KeyboardButton('Загрузить фото профиля'))
     bot.send_message(message.chat.id, text=f'Hi, {message.from_user.first_name}', reply_markup=keyboard)
 
 
 def after_text_2(message):
-    l.download_profile(message.text, profile_pic_only=True)
-    bot.send_message(message.chat.id, text=f'Вот Ваши подписчики:')
+    try:
+        driver.get("https://saveinsta.app/ru")
+        time.sleep(0.5)
+        text_bot = driver.find_element_by_name("q")
+        name_url = message.text
+        text_bot.send_keys(name_url)
+        time.sleep(0.5)
+        driver.find_element_by_class_name("input-group-btn").click()
+        time.sleep(0.5)
+        driver.find_element_by_id("closeModalBtn").click()
+        time.sleep(0.5)
+        driver.find_element_by_class_name("download-items__btn").click()
+        filename = glob.glob('/Users/aleksandramirnova/Downloads/*.mp4')
+        filename1 = filename[0]
+        print(filename1)
+        file = open(filename1, "rb")
+        bot.send_video(message.chat.id, file, "here it is!")
+        os.remove()
+    except Exception as es:
+        print('ex')
 
 @bot.message_handler(content_types=['text'])
 def get_answer(message):
-    if(message.text == 'Показать Ваших подписчиков'):
-        msg = bot.send_message(message.chat.id,text='Введите свой никнейм в Instagram',parse_mode="Markdown")
+    if(message.text == 'Загрузить фото профиля'):
+        msg = bot.send_message(message.chat.id,text='Введите ссылку в инстаграме',parse_mode="Markdown")
         bot.register_next_step_handler(msg, after_text_2)
 
 bot.polling(none_stop=True)
-
-
-
-
-
-
-
-        # for follower in followers:
-        #     user = bot.get_username_from_user_id(follower)
-#     arr[user] = {
-#         'user':user
-#     }
-
-# with open('arr.json', 'w') as file:
-#     json.dump(arr, file, indent=4, ensure_ascii=False)
-#     print(user)
 
