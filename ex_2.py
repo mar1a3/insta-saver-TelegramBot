80a5b1d41da0876bb87e3f3099cee10ce189eafd
capoweb <popytka.nomer1@bk.ru>
2023-06-30 17:26:11 +0000

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-ебучее программирование
 
 import time
 import glob
